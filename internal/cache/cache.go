37081443933e5607340b1276e924c03677b087af
bajiff <mr.baji.smart@gmail.com>
2026-07-17 22:29:39 +0700

    Lanjut hari ke 2

---
+++ internal/cache/cache.go
@@ -0,0 +1,31 @@
+package cache
+
+// 1. Buat Interface bernama 'Cache' dengan tipe Generic [V any].
+// Interface ini wajib memiliki 3 metode dasar:
+// - Set(key string, value V)
+// - Get(key string) (V, bool)
+// - Delete(key string)
+type Cache[V any] interface {
+	Set(key string, value V)
+	Get(key string) (V, bool)
+	Delete(key string)
+}
+
+// 2. Buat Struct bernama 'MemoryCache' dengan tipe Generic [V any].
+// Di dalam struct ini, buat satu properti bernama 'items' yang berupa map.
+// Map tersebut harus memiliki Key bertipe string, dan Value bertipe V.
+type MemoryCache[V any] struct {
+    // TUGAS ANDA: Deklarasikan properti 'items' berupa map di sini[cite: 51].
+		items map[string]V
+}
+
+// 3. Buat fungsi 'New' sebagai Constructor (Pabrik).
+// Fungsi ini bertugas mencetak objek MemoryCache baru yang siap pakai.
+// Ingat, map di Golang harus diinisialisasi dengan 'make' sebelum digunakan, 
+// jika tidak akan menyebabkan Panic (Fatal Error).
+func New[V any]() *MemoryCache[V] {
+    return &MemoryCache[V]{
+        // TUGAS ANDA: Inisialisasi properti 'items' menggunakan fungsi make()
+				items: make(map[string]V),
+    }
+}
--- logic.go
+++ logic.go
@@ -1,11 +1,1 @@
-// 2. INTERFACE (KONTRAK): Manajer restoran mensyaratkan setiap penyimpanan harus punya fitur 'Set' dan 'Get'.
-type StorageSOP[V any] interface {
-	Set(key string, value V)
-	Get(key string) (V, bool)
-}
-
-// 3. STRUCT + 4. GENERIC: Cetak biru wadah penyimpanan fisik yang mematuhi SOP di atas.
-// [V any] berarti "Value" bisa berupa tipe data apa saja (string, int, struct lain).
-type LaciAjaib[V any] struct {
-	Data map[string]V // Menggunakan Map sebagai penyimpanan In-Memory In-Built Go
-}
+package main
